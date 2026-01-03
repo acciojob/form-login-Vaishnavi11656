94db126a92e382d39340bb044b260bee03b27231
Vaishnavi11656 <166276503+Vaishnavi11656@users.noreply.github.com>
2026-01-04 01:30:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
     //Write your code here
+	const firstname = document.forms["form1"]["fname"].value.trim();
+	const lastname = document.forms["form2"]["lname"].value.trim();
+
+	const fullname = `${firstname} ${lastname}`.trim();
+
+	alert(fullname);
+
+	return false;
 
 }
