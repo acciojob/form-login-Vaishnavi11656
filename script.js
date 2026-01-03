f538100fc93f877aac20a546f7a74d0fe0471b9e
Vaishnavi11656 <166276503+Vaishnavi11656@users.noreply.github.com>
2026-01-04 01:32:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getFormvalue() {
     //Write your code here
 	const firstname = document.forms["form1"]["fname"].value.trim();
-	const lastname = document.forms["form2"]["lname"].value.trim();
+	const lastname = document.forms["form1"]["lname"].value.trim();
 
 	const fullname = `${firstname} ${lastname}`.trim();
 
